3f7b7037af14fe3e18f855b5128da5202e7efee6
Yusif <kerimov.y007@gmail.com>
2017-09-14 14:40:22 +0400

    Add files via upload

---
+++ getallfun.php
@@ -0,0 +1,21 @@
+<?php
+function fun($a)
+{
+	echo "phpdə olan bütün funskiyaların sayı:".'<em style="color:red">'.count($a["internal"]).'</em><br/>';
+    foreach($a as $k=>$v)
+    {
+    	$x=0;
+    	foreach ($v as $y) {
+    		$link=str_replace('_','-',$y );
+    		$link="http://php.net/manual/en/function.".$link.'.php';
+    		echo ++$x.")<a href=\"$link\">".$y.'() </a><br/>';
+    	}
+
+    }
+    echo "<br/><br/>";
+}
+
+$arr = get_defined_functions();
+unset($arr["user"]);
+fun($arr);
+?>
